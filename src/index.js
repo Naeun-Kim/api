c9d855e837d58c370fa737b63d1d158b10f81567
naeun kim <naeunkim@naeunui-MacBookAir.local>
2021-02-13 11:18:26 +0900

    connect MongoDB, separate graphQL's schema and resolver using apollo context

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 // index.js
 // This is the main entry point of our application
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
     { id: '1', content: 'This is a note', author: 'Adam Scott' },
@@ -11,47 +18,20 @@ let notes = [
     { id: '3', content: 'Oh hey look, another note', author: 'Lily' },
 ];
 
-const typeDefs = gql`
-    type Note {
-        id: ID!
-        content: String!
-        author: String!
-    }
-    type Query {
-        hello: String!
-        notes: [Note!]!
-        note(id: ID!): Note!
-    }
-    type Mutation {
-        newNote(content: String!): Note!
-    }
-`
-
-const resolvers = {
-    Query: {
-        hello: () => 'Hello World!',
-        notes: () => notes,
-        note: (parent, args) => {
-            return notes.find(note => note.id === args.id); 
-        }
-    },
-    Mutation: {
-        newNote: (parent, args) => {
-            let noteValue = {
-                id: String(notes.length + 1),
-                content: args.content,
-                author: 'Adam Scott'
-            };
-            notes.push(noteValue);
-            return noteValue;
-        }
-    }
-}
 
 const app = express();
+
+db.connect(DB_HOST);
+
 app.get('/', (req, res) => res.send('Hello Worldssss!!!!!!!'));
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context: () => {
+        return { models };
+    }
+});
 
 server.applyMiddleware({ app, path: '/api'});
 
